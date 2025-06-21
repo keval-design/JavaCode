65b48d1036623af5a69c79b1486b1c317f899fb7
keval-design <rathodkeval70901@gmail.com>
2025-06-21 15:07:56 +0530

    commit created

---
+++ Collection.java
@@ -0,0 +1,23 @@
+package day2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Collection {
+
+	public static void main(String[] args) {
+
+		  ArrayList<Integer> al = new ArrayList();
+		  
+	al.add(1);
+	al.add(2);
+	
+	
+	System.out.println(al);
+	
+	
+		
+	}
+
+}
